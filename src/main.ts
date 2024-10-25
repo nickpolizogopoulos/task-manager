895044e9626ede81cc9e5c7cfe52c9400952137d
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-10-25 10:25:10 +0300

    Lazy loading user tasks, new task

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding
+} from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
